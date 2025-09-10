42e30cc7503f842063fbf31289bd805d409dd1b2
Aatish Arote <130722414+aatisharote07@users.noreply.github.com>
2025-09-10 09:11:28 +0530

    Initial Commit

---
+++ Delete_Statement.sql
@@ -0,0 +1,9 @@
+-- USE my_firstdb;
+-- SELECT * FROM users WHERE dob < '1995-01-01';
+-- SELECT * FROM users WHERE id > 10
+--  UPDATE users
+--  SET name = 'Aisha Khan'
+--  WHERE email = 'aisha@example.com';
+--  SELECT * FROM users;
+DELETE FROM users
+WHERE id = 3;
---
+++ Where_Clause_Using_Operators.sql
@@ -0,0 +1,3 @@
+USE my_firstdb;
+SELECT * FROM users WHERE dob < '1995-01-01';
+SELECT * FROM users WHERE id > 10
