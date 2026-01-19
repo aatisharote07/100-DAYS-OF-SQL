2765efa30ddb691b816d8e8b244146f417f089bc
Aatish Arote <130722414+aatisharote07@users.noreply.github.com>
2026-01-19 08:39:35 +0530

    Rename Folders & Window Functions

---
+++ TASKS/WINDOW_FUNCTIONS/window_functions_02.sql
@@ -0,0 +1,33 @@
+-- Q5. For each patient, calculate the difference between their claimed amount 
+-- and the average claimed amount of patients with the same number of children
+
+SELECT *, 
+claim - AVG(claim) OVER(PARTITION BY children)
+FROM insurance;
+
+-- Q6. Show the patient with the highest BMI in each region and their respective overall rank
+SELECT * FROM (SELECT *,
+RANK() OVER(PARTITION BY region ORDER BY bmi DESC) AS group_rank,
+RANK() OVER(ORDER BY bmi DESC) AS overall_rank
+FROM insurance) t
+WHERE t.group_rank = 1;
+
+-- Q7. Calculate the difference between the claimed amount of each patient 
+-- and the claimed amount of the patient who has the highest BMI
+-- in their region
+SELECT *,
+claim - FIRST_VALUE(claim) OVER(PARTITION BY region ORDER BY bmi DESC)
+FROM insurance;
+
+-- Q8. For each patient, calculate the difference in claim amount between the patient and the patient
+-- with the highest claim amount among patients with same smoker status,
+-- within the same region. Return the result in descending order difference.
+SELECT *,
+(MAX(claim) OVER(PARTITION BY region,smoker) - claim) AS claim_diff
+FROM insurance 
+ORDER BY claim_diff DESC;
+
+-- Q9. For each patient, find the Maximum BMI value
+SELECT *, 
+MAX(bmi) OVER(ORDER BY age ROWS BETWEEN 1 FOLLOWING AND 3 FOLLOWING)
+FROM insurance
